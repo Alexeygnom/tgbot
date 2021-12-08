22b839309dcf97f805678dba020d8299caea956e
FX506 <astanchev_@miem.hse.ru>
2021-12-08 19:19:08 +0300

    b

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('5060294107:AAEmco-9d_24GT_gS9jepQ4VsxBUbKvAX5U')
 
@@ -9,11 +10,22 @@ COMMANDS = ['start', 'beginning1', 'beginning2', 'beginning3',
 
 def welcom(message):
     bot.reply_to(message,'Ещё один... ну привет. У меня один вопрос: ты азергиджанец?')
-    #a = message.text
-    #bot.reply_to(message, a)
-    #bot.reply_to(message, 'Готов начать?')
+    b = keybord1
+    if b == 'Да':
+        bot.reply_to(message, 'Юююю..... Окей, будешь холопом')
+    if b == 'Нет':
+        bot.reply_to(message, 'Отлично!')
 
-#def start_message(message) -> None:
+
+def keybord1():
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    btn1 = types.KeyboardButton('Да')
+    btn2 = types.KeyboardButton('Нет')
+    markup.add(btn1, btn2)
+    return markup
+
+
+
 
 
 
