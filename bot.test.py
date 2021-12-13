5880bf2e4a8089d073efc3545fc3b82dee3c577d
FX506 <astanchev_@miem.hse.ru>
2021-12-13 21:20:02 +0300

    e

--- bot.test.py
+++ bot.test.py
@@ -1,10 +1,26 @@
-import unittest
+from telethon import TelegramClient, sync, events
+import time
+#from DB import db
+# Use your own values from my.telegram.org
+api_id = '18305040'
+api_hash = 'e360f892de1940d08ff938b5819dffe8'
+client = TelegramClient('qwerty', api_id, api_hash)
 
+client.start()
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
+client.send_message('@Mind_gnom_bot', '/start')
+time.sleep(1)
+client.send_message('@Mind_gnom_bot', '/victorina')
+time.sleep(1)
+client.send_message('@Mind_gnom_bot', 'География')
+time.sleep(1)
+client.send_message('@Mind_gnom_bot', 'Либревиль')
+time.sleep(1)
+client.send_message('@Mind_gnom_bot', 'Молдова')
+time.sleep(1)
+client.send_message('@Mind_gnom_bot', 'Южного')
+time.sleep(1)
+client.send_message('@Mind_gnom_bot', 'Г')
+time.sleep(1)
+client.send_message('@Mind_gnom_bot', 'Антильское')
+time.sleep(1)
